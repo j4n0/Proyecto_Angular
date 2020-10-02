5798ee7eff5b4d8ed62192148cd23adfa50d8975
j4n0 <nelson.a.sanhueza@gmail.com>
2020-10-02 00:30:25 -0300

    header-body-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
 
-import { FooterComponent } from './components/footer/footer.component';
 /*Material Impornt*/
+//import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 
 import { MaterialModule } from './material/material.module';
-/*end Material Impornt*/
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
     FooterComponent
   ],
   imports: [
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styles: []
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
+  name ="Nelson Sanhueza";
+  emnail="nelson.a.sanhueza@gmail.com";
+  
   constructor() { }
 
   ngOnInit() {
