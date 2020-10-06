b2d9b2f45db69f9a6fcfdb3518e8ce7164f4a9ab
j4n0 <nelson.a.sanhueza@gmail.com>
2020-10-06 18:43:00 -0300

    Tarea 1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {InformacionService} from '../../services/informacion.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    
+    public _is:InformacionService
+
+  ) { }
 
   ngOnInit() {
   }
--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -9,16 +9,58 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class InformacionService {
 
   info:any ={};
-  cargada:boolean = false;
+  cargada_info:boolean = false;
+
+  familia:any={};
+  carga_familia:boolean = false;
+
+  home:any={};
+  carga_home:boolean=false;
 
   constructor( public http :HttpClient) {
 
+
+    this.carga_informacion ();
+    this.Carga_Familia();
+    this.Carga_Home();
+    
+   }
+
+   /**
+    * carga_informacion
+    */
+   public carga_informacion() {
     this.http.get("assets/Data/Info.pagina.json")
     .subscribe(data =>{console.log(data);
-      this.cargada = true;
+      this.cargada_info = true;
       this.info = (data);
+    
+    });
+   }
+
+   /**
+    * Carga_Familia
+    */
+   public Carga_Familia() {
+    this.http.get("assets/Data/prueba.json")
+    .subscribe(data =>{console.log(data);
+      this.carga_familia = true;
+      this.familia = (data);
     
     })
     
+     
+   }
+
+   /**
+    * Carga_Home
+    */
+   public Carga_Home() {
+    this.http.get("assets/Data/home.json")
+    .subscribe(data =>{console.log(data);
+      this.carga_home = true;
+      this.home = (data);
+    
+    })
    }
 }
